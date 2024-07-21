6d518260e47a874f533fe30849d14a4e9a967fdb
KaiThorpe <43959800+KaiThorpe@users.noreply.github.com>
2024-07-21 14:05:24 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,1 +1,1 @@
-const CONFIG_BACKEND_URL = 'https://osrs-gimp-tracker-backend-production.up.railway.app:8081'
+const CONFIG_BACKEND_URL = 'https://osrs-gimp-tracker-backend-production.up.railway.app'
